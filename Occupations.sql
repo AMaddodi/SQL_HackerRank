14e41fc8ffab8b868f70b798db5c8dbf165b802d
Archana Maddodi <134722015+AMaddodi@users.noreply.github.com>
2023-07-13 22:10:07 +0100

    Update Occupations.sql

--- Occupations.sql
+++ Occupations.sql
@@ -8,3 +8,41 @@ from        (select      name,
                         row_number() over (partition by occupation order by name) as rn
             from        occupations) as t
 group by rn;
+
+/*
+Using pivot table MS SQL
+*/
+select [Doctor], [Professor],[singer], [actor]
+from        (select      name,
+                        occupation,
+                        row_number() over (partition by occupation order by name) as rn
+            from        occupations) as t
+pivot
+( max(name) for occupation in ([Doctor],[Professor],[Singer],[Actor])
+) as pvt
+order by rn;
+
+/*
+Using case and temp table
+*/
+with temp_tbl as (
+    select
+        case
+            when occupation ='Doctor' then name else NULL end as Doctor, 
+        case
+            when occupation = 'Professor' then name else NULL end as Professor,
+        case
+            when occupation = 'Singer' then name else NULL end as Singer,
+        case    
+            when occupation = 'Actor' then Name else NULL end as Actor,
+        row_number() over (partition by occupation order by name) as rn
+    from occupations
+)
+
+select 
+            max(Doctor),
+            max(Professor),
+            max(Singer),
+            max(Actor)
+from        temp_tbl
+group by    rn;
